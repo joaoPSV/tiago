11630c6431c0593fb6e3c80939facb4469d07c09
joão pedro <joao.p1@hotmail.com>
2023-01-10 23:04:28 -0300

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,27 +9,27 @@ const connection = mysql.createConnection({
 });
 
 const create = async (id, name) => {
-  const sql = "INSERT INTO boilerplates(id,name) VALUES (?,?);";
+  const sql = "INSERT INTO users(id,name) VALUES (?,?);";
   return await connection.promise().query(sql, [id, name]);
 };
 
 const findAll = async () => {
-  const sql = "SELECT * FROM boilerplates;";
+  const sql = "SELECT * FROM users;";
   return await connection.promise().query(sql);
 };
 
 const findById = async (id) => {
-  const sql = "SELECT * FROM boilerplates WHERE id = ?;";
+  const sql = "SELECT * FROM users WHERE id = ?;";
   return await connection.promise().query(sql, [id]);
 };
 
 const deleteById = async (id) => {
-  const sql = "DELETE FROM boilerplates WHERE id = ?;";
+  const sql = "DELETE FROM users WHERE id = ?;";
   return await connection.promise().query(sql, [id]);
 };
 
 const updateById = async (id, name) => {
-  const sql = "UPDATE boilerplates SET name = ? WHERE id = ?;";
+  const sql = "UPDATE users SET name = ? WHERE id = ?;";
   return await connection.promise().query(sql, [name, id]);
 };
 
